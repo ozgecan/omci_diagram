617e9e9ac1ba42736d9d285b58f3435519ad108b
ozgecanetsia <ozge.ayaz@netsia.com>
2020-10-31 19:58:22 +0300

    added omci read and banner

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package omci_diagram
+package main
+
+import (
+	"fmt"
+	"github.com/CrowdSurge/banner"
+)
+
+func main() {
+	fmt.Print("OMCI Diagram")
+	createBanner()
+	scanFile("/home/ozgecan/CHT/1/omci.log")
+}
+func createBanner(){
+	banner.Print("omci diagram")
+}
--- readOmciFile.go
+++ readOmciFile.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func scanFile(logfile string) {
+	f, err := os.OpenFile(logfile, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Open file error: %v", err)
+		return
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		value := sc.Text()
+		fmt.Println("line", value)
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatalf("Scan file error: %v", err)
+		return
+	}
+}
